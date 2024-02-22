1ddcebe04151685a727e6452f8524c630bb42d94
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-22 11:48:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const textInput = document.getElementById("text")
+const delayInput = document.getElementById("delay")
+const button = document.getElementById("btn")
+const outputInput = document.getElementById("output")
+
+
+async function whensubmit() {
+    const text = textInput.value;
+    const delay = parseInt(delayInput.value)*1000;
+
+    await new Promise (resolve=> setTimeout (resolve, delay));
+
+    outputInput.innerText = text
+    
+
+}
+
+
+
+
+
+
+
+button.addEventListener("click", whensubmit)
